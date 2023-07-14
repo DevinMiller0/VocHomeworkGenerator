b05759a4535dc0c73e5398bd0adde513ceaf35cd
Bruce <hide@hide.com>
2023-07-14 15:36:27 +0800

    feat: define POS.

---
+++ src/POS.ts
@@ -0,0 +1,80 @@
+//  In this code, we have defined several lists that categorize different word types such as nouns, verbs, adjectives, etc. Each list contains a set of strings representing the abbreviations or full names of the corresponding word type.
+//   The comments added explain the purpose of each list and provide information about the abbreviations or full names associated with each word type.
+//   The module exports all the lists so that they can be used in other modules or files.
+
+// Define lists of different word types/POS.
+
+
+const NOUN_LIST = [
+  "n.",
+  "noun"
+]
+const VERB_LIST = [
+  "v.",
+  "vt.",
+  "vi.",
+  "verb"
+]
+
+const ADJECTIVE_LIST = [
+  "adj.",
+  "adjective"
+]
+
+const ADVERB_LIST = [
+  "adv.",
+  "adverb"
+]
+
+const PREPOSITION_LIST = [
+  "prep.",
+  "preposition"
+]
+
+const CONJUNCTION_LIST = [
+  "conj.",
+  "conjunction"
+]
+
+const INTERJECTION_LIST = [
+  "interj.",
+  "interjection"
+]
+
+const PRONOUN_LIST = [
+  "pron.",
+  "pronoun"
+]
+
+const ARTICLE_LIST = [
+  "art.",
+  "article"
+]
+
+const ABBREVIATION_LIST = [
+  "abbr.",
+  "abbreviation"
+]
+
+const PREFIX_LIST = [
+  "prefix"
+]
+
+const SUFFIX_LIST = [
+  "suffix"
+]
+
+module.exports = {
+  NOUN_LIST,
+  VERB_LIST,
+  ADJECTIVE_LIST,
+  ADVERB_LIST,
+  PREPOSITION_LIST,
+  CONJUNCTION_LIST,
+  INTERJECTION_LIST,
+  PRONOUN_LIST,
+  ARTICLE_LIST,
+  ABBREVIATION_LIST,
+  PREFIX_LIST,
+  SUFFIX_LIST
+}
